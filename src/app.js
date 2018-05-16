b625c9a62cbc71549c77acd6b40a704e2510cf87
dmaflab <danielmaflab@hotmail.com>
2018-05-16 08:40:12 -0500

    cambiar puerto a 9000

--- src/app.js
+++ src/app.js
@@ -7,23 +7,23 @@ const app = express();
 
 // connection to db
 mongoose.connect('mongodb://localhost/crud-mongo')
-  .then(db => console.log('db connected'))
-  .catch(err => console.log(err));
+    .then(db => console.log('db connected'))
+    .catch(err => console.log(err));
 
 // importing routes
 const indexRoutes = require('./routes/index');
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 9000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // middlewares
 app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}))
-// routes
+app.use(express.urlencoded({ extended: false }))
+    // routes
 app.use('/', indexRoutes);
 
 app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`);
+    console.log(`server on port ${app.get('port')}`);
 });
