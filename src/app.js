663d1411ba4d79a20000fd2f51991d3f622e0b6d
dmaflab <danielmaflab@hotmail.com>
2018-05-16 21:09:41 -0500

    para subir a openshift

--- src/app.js
+++ src/app.js
@@ -4,9 +4,16 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
+db_name = 'crud-mongo'
+    //provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
 
 // connection to db
-mongoose.connect('mongodb://localhost/crud-mongo')
+mongoose.connect(mongodb_connection_string)
     .then(db => console.log('db connected'))
     .catch(err => console.log(err));
 
@@ -14,7 +21,7 @@ mongoose.connect('mongodb://localhost/crud-mongo')
 const indexRoutes = require('./routes/index');
 
 // settings
-app.set('port', process.env.PORT || 9000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 9000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -24,6 +31,9 @@ app.use(express.urlencoded({ extended: false }))
     // routes
 app.use('/', indexRoutes);
 
-app.listen(app.get('port'), () => {
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP ||
+    'localhost'
+
+app.listen(app.get('port'), server_ip_address, () => {
     console.log(`server on port ${app.get('port')}`);
 });
